32120bffbf9d97c2294385ccd55a03aa79afd580
Richard Braamburg <richard.braamburg@gmail.com>
2020-02-11 10:11:07 -0500

    minor tweaks

--- backend/app/controllers/users_controller.rb
+++ backend/app/controllers/users_controller.rb
@@ -6,26 +6,10 @@ class UsersController < ApplicationController
   def create
     user = User.create!(user_params)
     auth_token = AuthenticateUser.new(user.email, user.password).call
-    response = {message: Message.account_created, auth_token: auth_token}
+    response = {message: Message.account_created, auth_token: auth_token, user: user}
     json_response(response, :created)
     UserMailer.welcome_email(user).deliver
   end
-
-  # def generate_password_token!
-  #   self.reset_password_token = generate_token
-  #   self.reset_password_sent_at = Time.now.utc
-  #   save!
-  # end
-  #
-  # def password_token_valid?
-  #   (self.reset_password_sent_at + 4.hours) > Time.now.utc
-  # end
-  #
-  # def reset_password!(password)
-  #   self.reset_password_token = nil
-  #   self.password = password
-  #   save!
-  # end
 
   private
 
@@ -37,9 +21,5 @@ class UsersController < ApplicationController
         :password_confirmation
     )
   end
-
-  # def generate_token
-  #   SecureRandom.hex(10)
-  # end
 
 end
