6fa1503a87a73f6e642856213e1b5891f571da53
Richard Braamburg <richard.braamburg@gmail.com>
2020-02-15 13:34:46 -0500

    Created ChatBoard component

--- frontend/src/components/ChatBoard.js
+++ frontend/src/components/ChatBoard.js
@@ -1,17 +1,42 @@
-import React from "react";
+import React, { Component } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faComments } from "@fortawesome/free-solid-svg-icons";
-
 const chat = <FontAwesomeIcon icon={faComments} />;
 
-const ChatBoard = () => {
-  return (
-    <div className="chatBoard">
-      <iframe url="http://localhost:4000/"></iframe>
-      <button className="toggleChat"></button>
-      <i className="toggleChat">{chat}</i>
-    </div>
-  );
-};
+class ChatBoard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  chatToggle = () => {
+    let chatApp = document.querySelector("iframe");
+    let chatIcon = document.querySelector("i");
+    chatApp.classList.contains("is-active")
+      ? chatApp.classList.remove("is-active")
+      : chatApp.classList.add("is-active");
+    chatApp.classList.contains("not-active")
+      ? chatApp.classList.remove("not-active")
+      : chatApp.classList.add("not-active");
+    chatIcon.classList.contains("grayed-out")
+      ? chatIcon.classList.remove("grayed-out")
+      : chatIcon.classList.add("grayed-out");
+  };
+
+  render() {
+    return (
+      <div className="chatBoard">
+        <iframe
+          className="chatApp not-active"
+          url="http://localhost:4000/"
+        ></iframe>
+        <button className="toggleChat"></button>
+        <i onClick={() => this.chatToggle()} className="toggleChat">
+          {chat}
+        </i>
+      </div>
+    );
+  }
+}
 
 export default ChatBoard;
