e489c445e654003da7646de241eb3454249619bc
Richard Braamburg <richard.braamburg@gmail.com>
2020-02-15 13:36:00 -0500

    New Link and Route to Comments and Create Comment

--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -1,18 +1,52 @@
 import React, { Component } from "react";
+import ReactPlayer from "react-player";
+import { Link, withRouter } from "react-router-dom";
+import { verifyUser, postsAll } from "../services/api_helper";
 
 class Main extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      posts: null
+    };
   }
 
+  componentDidMount = () => {
+    verifyUser();
+    this.loadPosts();
+  };
+
+  loadPosts = async () => {
+    const posts = await postsAll();
+    console.log(posts);
+    this.setState({
+      posts
+    });
+  };
+
   render() {
+    console.log(this.state.posts);
     return (
-      <main>
-        <h1>We're alive!</h1>
-      </main>
+      <section className="main_container">
+        {this.state.posts &&
+          this.state.posts.map((post, key) => (
+            <div key={key} className="ind_vid_container">
+              <Link to={`/video/${post.id}`}>
+                <h4>{post.title}</h4>
+              </Link>
+              <ReactPlayer
+                // playing
+                url={post.video_url}
+                config={{
+                  youtube: { playerVars: { controls: 0, modestbranding: 1 } }
+                }}
+              />
+              <p>{post.description}</p>
+            </div>
+          ))}
+      </section>
     );
   }
 }
 
-export default Main;
+export default withRouter(Main);
