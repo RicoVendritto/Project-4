1790f21d79e05d24b224218a4d09798bd1c9e6dd
Richard Braamburg <richard.braamburg@gmail.com>
2020-02-16 13:09:45 -0500

    Rerouting login validations

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
     resources :comments
   end
 
+  # post 'login', to: 'users#login'
   post 'auth/login', to: 'authentication#authenticate'
   post 'signup', to: 'users#create'
 
