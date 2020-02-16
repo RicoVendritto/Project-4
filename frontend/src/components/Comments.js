ea989aced2c22b291cad267eafa02a1845381394
Richard Braamburg <richard.braamburg@gmail.com>
2020-02-15 22:07:37 -0500

    Introducting days per post and delete function

--- frontend/src/components/Comments.js
+++ frontend/src/components/Comments.js
@@ -5,7 +5,8 @@ import CreateComment from "./CreateComment";
 import {
   commentsPost,
   commentCreate,
-  verifyUser
+  verifyUser,
+  commentDelete
 } from "../services/api_helper";
 
 class Comments extends Component {
@@ -13,7 +14,8 @@ class Comments extends Component {
     super(props);
     this.state = {
       comments: null,
-      apiDataLoaded: false
+      apiDataLoaded: false,
+      user_id: null
     };
   }
 
@@ -21,31 +23,61 @@ class Comments extends Component {
     verifyUser();
     const comments = await commentsPost(this.props.vid_id);
     this.setState({
-      comments: comments,
-      apiDataLoaded: true
+      comments,
+      apiDataLoaded: true,
+      user_id: localStorage.getItem("id")
     });
   };
 
   handleComment = async (e, vidId, commentData) => {
     e.preventDefault();
     const comment = await commentCreate(vidId, commentData);
+    console.log(comment);
     this.setState({
       comments: [...this.state.comments, comment]
     });
   };
 
+  deleteComment = async (e, vidId) => {
+    e.preventDefault();
+    const comment = await commentDelete(this.props.vid_id, vidId);
+    console.log(comment);
+  };
+
+  countDays = postDate => {
+    let now = new Date();
+    let post = new Date(postDate);
+    let diff_time = now.getTime() - post.getTime();
+    let diff_days = Math.floor(diff_time / (1000 * 3600 * 24));
+    return diff_days;
+  };
+
   render() {
     console.log(this.props.vid_id);
-    console.log(this.state.comments);
+    console.log(this.state);
     console.log(localStorage);
     return (
       <>
         <h2>COMMENTS</h2>
         {this.state.apiDataLoaded &&
           this.state.comments.map((comment, id) => (
-            <div key={id}>
+            <div key={id} className="ind_comment">
               <p>{comment.comment}</p>
-              <p>{comment.created_by}</p>
+              <h5>{comment.user_name}</h5>
+              <h6>
+                Posted {this.countDays(comment.created_at.split("T")[0])} days
+                ago
+              </h6>
+              {parseInt(this.state.user_id) === parseInt(comment.created_by) ? (
+                <div className="comment_options">
+                  <button>EDIT</button>
+                  <button onClick={e => this.deleteComment(e, comment.id)}>
+                    DELETE
+                  </button>
+                </div>
+              ) : (
+                <div className="comment_options">NO OPTIONS</div>
+              )}
             </div>
           ))}
         <CreateComment
